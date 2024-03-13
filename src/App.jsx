e050e7479b0777a96b7cfcc7c859417cf50f0721
Rory Simmonds <rory@blankbox.business>
2024-03-13 21:14:26 +0000

    initial file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Quote from './components/Quote';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1>
         Shakespeers
       </h1>
+      <Quote/>
     </>
   )
 }
