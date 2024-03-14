a7986aecc3d8cc59eae65f229d1ecf9f006945cc
Rory Simmonds <rory@blankbox.business>
2024-03-14 19:00:24 +0000

    add homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Quote from './components/Quote';
+import Quote from './components/Quote'
+import Homepage from './components/Homepage'
 
 
 function App() {
 
   return (
     <>
-      <h1>
-        Shakespeers
-      </h1>
-      <Quote/>
+      <Homepage/>
     </>
   )
 }
