5821aed43871b88def5a25080084bb01ed9d860c
teeteetian <Teetee_tian@outlook.com>
2024-03-15 19:56:32 +0000

    Added starter layout for homepage components

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,12 +1,44 @@
 import React from "react"
 import './style.css'
 
+
+
+
 function Homepage() {
   return (
+
+<div className="container">
     <div>
-      <h1>Test</h1>
-      <p>Test text</p>
+    <img src="./src/assets/happ-speare.png" className="logoFace"></img>
+    <img src="./src/assets/logo.png" className="logoText"></img>
+      <p>Welcome to the ShakesPeers revision App</p>
     </div>
+
+    <div>
+
+    <div className="dropdown">
+  <button className="dropbtn">Select your play here</button>
+  <div className="dropdown-content">
+    <a href="#">Play 2</a>
+    <a href="#">Play 2</a>
+    <a href="#">Play 3</a>
+  </div>
+</div>
+    </div>
+
+    <div>
+    <h1></h1>
+    <textarea rows="1" cols="50">
+Enter your name here...
+</textarea>
+    </div>
+
+    <div>    
+      <h3>Watch the Tutorial here</h3> 
+      </div>
+
+    <button> See My Saved Quotes</button>
+</div>
   )
 }
 
