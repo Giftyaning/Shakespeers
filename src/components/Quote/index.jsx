7608a8566275e92b201091557fe8dc76ac692b8f
Kanika Aggarwal <kanikaaggarwal22@gmail.com>
2024-03-20 20:40:16 +0000

    Merge branch 'main' into generatePDF

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -150,6 +150,43 @@ const Quote = () => {
                                     <img
                                       id="happy"
                                       className="h-10 w-10 rounded-full"
+                                      src="./src/assets/happ-speare.png">
+                                    </img>
+                                  </div>
+                                  <div className="ml-3 flex-1">
+                                    <p className="text-sm font-medium text-gray-900">
+                                      Contextual Details
+                                    </p>
+                                    <p className="mt-1 text-sm text-gray-500">
+                                      Sure! 8:30pm works great!
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="flex border-l border-gray-200">
+                                <button
+                                  onClick={() => toast.dismiss(t.id)}
+                                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                >
+                                  Close
+                                </button>
+                              </div>
+                            </div>
+                          ))
+                        } } className="btn rounded-pill bg-primary text-light help-btn">
+                        <button type="button" onClick={()=>{
+                          toast.custom((t) => (
+                            <div
+                              className={`${
+                                t.visible ? 'animate-enter' : 'animate-leave'
+                              } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+                            >
+                              <div className="flex-1 w-0 p-4">
+                                <div className="flex items-start">
+                                  <div className="flex-shrink-0 pt-0.5">
+                                    <img
+                                      id="happy"
+                                      className="h-10 w-10 rounded-full"
                                       src="https://github.com/Giftyaning/Shakespeers/blob/main/src/assets/happ-speare.png?raw=true">
                                     </img>
                                   </div>
@@ -210,7 +247,7 @@ const Quote = () => {
                                     <img
                                       id="happy"
                                       className="h-10 w-10 rounded-full"
-                                      src="https://github.com/Giftyaning/Shakespeers/blob/main/src/assets/happ-speare.png?raw=true">
+                                      src="./src/assets/happ-speare.png">
                                     </img>
                                   </div>
                                   <div className="ml-3 flex-1">
@@ -222,19 +259,18 @@ const Quote = () => {
                                     </p>
                                   </div>
                                 </div>
-                                <div className="flex border-l border-gray-200">
-                                  <button
-                                    onClick={() => toast.dismiss(t.id)}
-                                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                                  >
-                                    Close
-                                  </button>
-                                </div>
                               </div>
-                            ));
-                          }}
-                          className="btn rounded-pill bg-primary text-light help-btn"
-                        >
+                              <div className="flex border-l border-gray-200">
+                                <button
+                                  onClick={() => toast.dismiss(t.id)}
+                                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                >
+                                  Close
+                                </button>
+                              </div>
+                            </div>
+                          ))
+                        } } className="btn rounded-pill bg-primary text-light help-btn">
                           <i className="fa-solid fa-question"></i>
                         </button>
                       </div>
@@ -270,7 +306,7 @@ const Quote = () => {
                                     <img
                                       id="happy"
                                       className="h-10 w-10 rounded-full"
-                                      src="https://github.com/Giftyaning/Shakespeers/blob/main/src/assets/happ-speare.png?raw=true">
+                                      src="./src/assets/happ-speare.png">
                                     </img>
                                   </div>
                                   <div className="ml-3 flex-1">
@@ -282,19 +318,18 @@ const Quote = () => {
                                     </p>
                                   </div>
                                 </div>
-                                <div className="flex border-l border-gray-200">
-                                  <button
-                                    onClick={() => toast.dismiss(t.id)}
-                                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                                  >
-                                    Close
-                                  </button>
-                                </div>
                               </div>
-                            ));
-                          }}
-                          className="btn rounded-pill bg-primary text-light help-btn"
-                        >
+                              <div className="flex border-l border-gray-200">
+                                <button
+                                  onClick={() => toast.dismiss(t.id)}
+                                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                >
+                                  Close
+                                </button>
+                              </div>
+                            </div>
+                          ))
+                        } } className="btn rounded-pill bg-primary text-light help-btn">
                           <i className="fa-solid fa-question"></i>
                         </button>
                       </div>
